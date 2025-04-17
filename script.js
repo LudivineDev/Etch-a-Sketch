36765d10c19c037c540550e57f4fab19368f3976
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-17 14:30:12 -0700

    square mainly done

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".grid-container");
+const gridSize = 16;
+const totalSquares =  gridSize * gridSize;
+
+for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement(`div`);
+    square.classList.add(`grid-square`);
+    container.appendChild(square);
+    
+}
