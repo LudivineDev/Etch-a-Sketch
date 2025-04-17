a0ea027f68ec9422cdff9eba2828f38332346c33
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-17 14:52:33 -0700

    hover with trail

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement(`div`);
     square.classList.add(`grid-square`);
     container.appendChild(square);
+
+     //  Add the trail effect
+     square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'violet'; // or random color
+      });
     
-}
+      container.appendChild(square);
+    }
+
